73eedf5ccffde9cc855beb751f9dcd769c42bbd6
TJ Holowaychuk <tj@vision-media.ca>
2014-09-19 18:54:48 -0700

    add response size to log output

--- logger.go
+++ logger.go
@@ -1,5 +1,6 @@
 package logger
 
+import "github.com/dustin/go-humanize"
 import "github.com/segmentio/go-log"
 import "net/http"
 import "time"
@@ -7,7 +8,8 @@ import "time"
 // wrapper to capture status.
 type wrapper struct {
 	http.ResponseWriter
-	status int
+	written int
+	status  int
 }
 
 // capture status.
@@ -16,15 +18,23 @@ func (w *wrapper) WriteHeader(code int) {
 	w.ResponseWriter.WriteHeader(code)
 }
 
+// capture written bytes.
+func (w *wrapper) Write(b []byte) (int, error) {
+	n, err := w.ResponseWriter.Write(b)
+	w.written += n
+	return n, err
+}
+
 // New logger middleware.
 func New() func(http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			start := time.Now()
-			res := &wrapper{w, 200}
+			res := &wrapper{w, 0, 200}
 			log.Info(">> %s %s", req.Method, req.RequestURI)
 			h.ServeHTTP(res, req)
-			log.Info("<< %s %s %d in %s", req.Method, req.RequestURI, res.status, time.Since(start))
+			size := humanize.Bytes(uint64(res.written))
+			log.Info("<< %s %s %d (%s) in %s", req.Method, req.RequestURI, res.status, size, time.Since(start))
 		})
 	}
 }
